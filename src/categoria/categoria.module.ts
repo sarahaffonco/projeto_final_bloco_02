0a27bab102257e57ef1ab0c4f1772079ed3af845
Sarah Affonco <sarah.affonco@gmail.com.br>
2025-08-12 12:15:52 -0300

    implementado CRUD

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -1,11 +1,14 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { Categoria } from './entities/categoria.entity';
+import { CategoriaService } from './services/categoria.service';
+import { CategoriaController } from './controllers/categoria.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Categoria])],
-  controllers: [],
-  providers: [],
+  providers: [CategoriaService],
+
+  controllers: [CategoriaController],
   exports: [],
 })
 export class CategoriaModule {}
